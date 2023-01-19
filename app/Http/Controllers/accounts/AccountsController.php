fb5d87e690c50520fc5b2ff96a5127dc6caa9202
brianchemase <brianchemo@gmail.com>
2023-01-19 15:33:27 +0300

    files updated

--- app/Http/Controllers/accounts/AccountsController.php
+++ app/Http/Controllers/accounts/AccountsController.php
@@ -11,7 +11,27 @@ class AccountsController extends Controller
     //
     public function index()
     {
-        return view('layouts.accounts.home');
+        $montly_repayments="";
+
+        $montly_repayments="250000";
+        $lastMontRepayments="350000";
+        $clients_counts="125";
+
+        $mpesa_repayments=2000000;
+        $bank_repayments=1000000;
+        $expected_repayment=4000000;
+        $pending_repayment=$expected_repayment-($mpesa_repayments+$bank_repayments);
+
+
+
+
+
+
+        return view('accounts.home' , 
+        compact('montly_repayments', 'lastMontRepayments', 'clients_counts', 
+        'mpesa_repayments','bank_repayments','expected_repayment','pending_repayment'
+    
+    ));
     }
 
     public function blank()
