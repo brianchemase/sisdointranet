af32f7545eac78bd9396e15f6d6d1870e5be1e79
brianchemase <brianchemo@gmail.com>
2023-02-08 22:22:34 +0300

    added the staf register tab

--- resources/views/accounts/stafflist.blade.php
+++ resources/views/accounts/stafflist.blade.php
@@ -11,6 +11,69 @@
                         For more information about DataTables, please visit the <a target="_blank"
                             href="https://datatables.net">official DataTables documentation</a>.</p>
 
+<div class="container">
+
+
+                            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Register Staff
+</button>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Register Sisdo Staff</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form>
+        <div class="row">
+            <div class="col">
+            <input type="text" class="form-control" placeholder="First name">
+            </div>
+            <div class="col">
+            <input type="text" class="form-control" placeholder="Last name">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col">
+            <input type="text" class="form-control" placeholder="email Address">
+            </div>
+            <div class="col">
+            <input type="text" class="form-control" placeholder="phone">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col">
+            <input type="text" class="form-control" placeholder="Branch">
+            </div>
+            <div class="col">
+            <input type="text" class="form-control" placeholder="Department UNIT">
+            </div>
+        </div>
+        </form>
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+<br>
+
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
